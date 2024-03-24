3b5b59b4ef491d21d6a1f1afac982448f886ca73
HarrisonTemple <158719617+HarrisonTemple@users.noreply.github.com>
2024-03-24 20:48:18 +0100

    small changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests.exceptions
 from dotenv import load_dotenv
 import os
 import telebot
@@ -8,23 +9,23 @@ load_dotenv("secrets.env")
 TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
+bot.polling(non_stop=True)
+
+#TODO: preload all possible exchange pairs(?) or make a small list by hand (bad)
 
 @bot.message_handler(commands=["help"])
 def handle_help(message: telebot.types.Message):
-    bot.reply_to(message, f"To convert currency, type the command /convert , \n "
+    bot.reply_to(message, f"To convert currency, type the command /convert , "
                           f"symbol of a source currency, followed by the symbol of a destination currency and amount ")
 
 @bot.message_handler(commands=["convert"])
 def handle_convert_request(message: telebot.types.Message):
     contents = message.text.upper().split(" ")
-    # "command" "USD" "USD" ("1" optional)
+    # ["command", "USD", "USD", ("1" optional), ...]
     try:
         src_sym = contents[1]
         dst_sym = contents[2]
-        if len(contents) >= 4:
-            amount = float(contents[3])
-        else:
-            amount = 1
+        amount = float(contents[3]) if len(contents) >= 4 else 1
     except (TypeError, IndexError):
         bot.reply_to(message, "wrong format, please use /help command for more information ")
         return
@@ -34,8 +35,9 @@ def handle_convert_request(message: telebot.types.Message):
     except helpers.Exceptions.ProviderConversionError as e:
         bot.reply_to(message, str(e))
         return
+    except requests.exceptions.HTTPError as e:
+        bot.reply_to(message, "provider error: " + str(e))
+        return
 
     bot.reply_to(message, str(rate * amount))
 
-
-bot.polling(non_stop=True)
